f02a121106b38d88a81fee1d790545650a8e0c5e
Nergez <zhap043@live.rhul.ac.uk>
2020-06-07 14:35:38 +0000

    Predicted velocity as a function of radius.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-f = open("values.txt", "r") 
+f = open("values.txt", "r") ## opens up and reads the file
 f.readline()
-x = []
-y = []
+
+r = []
+v = []
+dR = []
+dV = []
+m = []
+predictedV = []
+G = 4.3*(10**-6)
 
 for line in f:
-  x.append(float(line.split('\t')[1]))
-  y.append(float(line.split('\t')[0]))
+  r.append(float(line.split("\t")[0]))
+  v.append(float(line.split("\t")[1]))
+  dR.append(line.split("\t")[2])
+  dV.append(float(line.split("\t")[3]))
+  m.append(float(line.split("\t")[4]))
+  predictedV.append(((float(G))*(float(line.split('\t')[4]))/(float(line.split('\t')[0])))**(1/2))
+
+x = np.array(r)
+y = np.array(v)
+y1 = np.array(predictedV)
 
 plt.plot(x,y)
-plt.xlabel('Radius(kpc)')
-plt.ylabel('Velocity(km/h)')
+plt.plot(x,y1)
 plt.show()
-
